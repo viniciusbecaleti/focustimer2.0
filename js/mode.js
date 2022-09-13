7131765d328cd44d08931ccc7c2423b44c07f020
Vinicius Becaleti <vinicius@becaleti.com>
2022-09-13 19:10:40 -0300

    mode.js refatorado

--- js/mode.js
+++ js/mode.js
@@ -3,23 +3,13 @@ export default function Mode({
   darkButton
 }) {
   function darkMode() {
-    document.documentElement.style.setProperty("--text-color", "white")
-    document.documentElement.style.setProperty("--button-bg", "#29292E")
-    document.documentElement.style.setProperty("--selected-button-bg", "#0A3442")
-    document.documentElement.style.setProperty("--body-bg", "#121214")
-    document.documentElement.style.setProperty("--button-color", "#C4C4CC")
-
+    document.body.classList.add('dark')
     lightButton.classList.add('hidden')
     darkButton.classList.remove('hidden')
   }
 
   function lightMode() {
-    document.documentElement.style.setProperty("--text-color", "#323238")
-    document.documentElement.style.setProperty("--button-bg", "#E1E1E6")
-    document.documentElement.style.setProperty("--selected-button-bg", "#02799D")
-    document.documentElement.style.setProperty("--body-bg", "white")
-    document.documentElement.style.setProperty("--button-color", "#323238")
-
+    document.body.classList.remove('dark')
     darkButton.classList.add('hidden')
     lightButton.classList.remove('hidden')
   }
